f2fdd3106a6e30ae25a87a40fc34a8e5b68f9d9e
saplx <shukodji@gmail.com>
2025-06-16 04:33:37 +0300

    add sidebar and i18n for localization

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -2,6 +2,8 @@ import { classNames } from "shared/lib/classNames/classNames"
 import cls from './Sidebar.module.scss'
 import { useState } from "react";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher";
+import { Button } from "shared/ui/Button/Button";
+import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
 
 
 interface SidebarProps {
@@ -17,9 +19,10 @@ export const Sidebar = ({ className }: SidebarProps) => {
 
   return (
     <div className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}>
-      <button onClick={onToggle}>toggle</button>
+      <Button onClick={onToggle}>toggle</Button>
       <div className={cls.switchers}>
         <ThemeSwitcher />
+        <LangSwitcher className={cls.lang} />
       </div>
     </div>
   )
